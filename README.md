f326d3ef42df80ecf704fa92e60ea62c85afb2dd
douglas.mwangi@africastalking.com <douglas.mwangi@africastalking.com>
2022-09-21 14:09:28 +0300

    few updates

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # AT-Airtime-Vendor
+
 <a href="https://heroku.com/deploy?template=https://github.com/DaggieBlanqx/AT-Airtime-Vendor">
   <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
 </a>
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const config = require('config');
+const WEB_APP = config.get('WEB_APP');
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .listen(WEB_APP.PORT, () => console.log(`Listening on ${WEB_APP.PORT}`));
