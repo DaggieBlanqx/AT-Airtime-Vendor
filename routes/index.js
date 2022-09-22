2431f99e9f7069430a6e2bbd0c9922c07cfe777c
loisek <loise.kimwe@africastalking.com>
2022-09-22 08:14:07 +0300

    airtime vendor code base

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,37 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const data = require('../data_store/logs.json');
 
 const {
     sendAirtime,
     doesFileExist,
     createBusinessOwnerFile,
 } = require('../utils/index');
-
+let analyticsInfo = ()=> {
+    // console.log(data);
+    let finalData = data.map(element => {
+        let fragData = element.responses;
+        // console.log('fragdata', fragData);
+        let result = fragData.map((el) => {
+            // console.log('element', el);
+            let elem = {...el}
+            return elem;
+        });
+        console.log('result', result);
+        // console,log('amaount', fragData.amount);
+        // let result = [...fragData];
+        return  result;
+    });
+    // console.log('responses',...finalData);
+    // console.log('json', JSON.stringify(finalData))
+}
 router.get('/', (req, res) => res.render('pages/index'));
+router.get('/credentials', (req, res) => res.render('pages/credentials'));
+router.get('/airtime', (req, res) => res.render('pages/airtime'));
+router.get('/analytics', (req, res) => res.render('pages/analytics', {
+    responseData: analyticsInfo(),
+}));
 
 router.get('/send/:amount/:phoneNumber', async (req, res) => {
     let phoneNumber = req.params.phoneNumber;
